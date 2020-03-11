7b0b9c964cc29c456530485f7438520f44d5d1eb
Guillermo <guillermotrabes@gmail.com>
2020-03-11 13:38:27 -0400

    sequential version working

--- example.cpp
+++ example.cpp
@@ -8,8 +8,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+#include <iostream>
 
-int main(void) {
-	puts("!!!Hello World!!!");
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	double *x,*y, partial_result, result;
+	int P, n;
+
+	P=atoi(argv[1]);
+	n=atoll(argv[2]);
+
+	x = (double*)malloc(n*sizeof(double));
+	y = (double*)malloc(n*sizeof(double));
+
+	/* initialization */
+
+	for(int i=0;i<n; i++){
+		x[i]=1.0;
+		y[i]=1.0;
+	}
+
+	/* inner product */
+
+	for(int i=0;i<n; i++){
+		partial_result+=x[i]*y[i];
+	}
+
+	cout << "Result:" << partial_result << endl;
+
 	return EXIT_SUCCESS;
 }
