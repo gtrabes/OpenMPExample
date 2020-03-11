c4c31eca4079cddf523346c3f27771706546dcfd
gtrabes <45105691+gtrabes@users.noreply.github.com>
2020-03-11 13:16:08 -0400

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : example.cpp
+// Author      : Guillermo Trabes
+// Version     :
+// Copyright   : * Copyright (c) 2020, Guillermo G. Trabes * Carleton University, Universite de Nice-Sophia Antipolis* All rights reserved.
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
